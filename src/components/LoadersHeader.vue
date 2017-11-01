e91f3e5ca293ba5c4fbdf80636b9655ccc8f2aa0
George M <georgezagraid@gmail.com>
2017-11-02 00:04:16 +0300

    Set up project structure and routes

---
+++ src/components/LoadersHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    this HEADER!
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'loaders-header',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style  lang="scss">
+
+</style>
