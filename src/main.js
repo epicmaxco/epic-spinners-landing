c1318ed3ffaf92dcf97357af0dc40bc962eb4dc0
alexanderrudnik <alexanderrud0603@gmail.com>
2019-10-14 14:46:57 +0300

    feat(app): set up gtm

--- src/main.js
+++ src/main.js
@@ -5,12 +5,19 @@ import App from './App'
 import router from './router'
 import Toasted from 'vue-toasted'
 import VueClipboard from 'vue-clipboard2'
+import VueGtm from 'vue-gtm'
 // import Vuetify from 'vuetify'
 //
 // Vue.use(Vuetify)
 Vue.use(VueClipboard)
 Vue.use(Toasted)
 Vue.config.productionTip = false
+
+Vue.use(VueGtm, {
+  id: 'GTM-K7BSQHC',
+  enabled: process.env.NODE_ENV === 'production',
+  vueRouter: router
+})
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import LoadersGetStarted from '@/components/LoadersGetStarted'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
